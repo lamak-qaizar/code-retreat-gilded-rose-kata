a1b3a43a96aefdb38548575a9762f1c08df2f643
Muhammad Aamir <>
2020-07-24 17:19:48 +0500

    extract methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,45 +8,68 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item currentItem = items[i];
-            if (nameNotEqual(currentItem, Constant.AGED_BRIE)
-                    && nameNotEqual(currentItem, Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
-                    && qualityGreaterThan(currentItem)
-                    && nameNotEqual(currentItem, Constant.SULFURAS_HAND_OF_RAGNAROS)) {
+            if (isSpecialItem(currentItem)) {
                 currentItem.quality = currentItem.quality - 1;
             } else {
-                if (isQualityLessThan50(currentItem.quality, 50)) {
-                    currentItem.quality = currentItem.quality + 1;
-                    if (currentItem.name.equals(Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
-                            && isSellInLessThan(currentItem.sellIn, 11)
-                            && isQualityLessThan50(currentItem.quality, 50)) {
-                        currentItem.quality = currentItem.quality + 1;
-                        if (isSellInLessThan(currentItem.sellIn, 6) && isQualityLessThan50(currentItem.quality, 50)) {
-                            currentItem.quality = currentItem.quality + 1;
-                        }
-                    }
-                }
+                handleQualityForNormalItem(currentItem);
             }
-            if (nameNotEqual(currentItem, Constant.SULFURAS_HAND_OF_RAGNAROS)) {
-                currentItem.sellIn = currentItem.sellIn - 1;
-            }
-            if (isSellInLessThan(currentItem.sellIn, 0)
-                    && nameNotEqual(currentItem, Constant.AGED_BRIE)
-                    && nameNotEqual(currentItem, Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
-                    && qualityGreaterThan(currentItem)
-                    && nameNotEqual(currentItem, Constant.SULFURAS_HAND_OF_RAGNAROS)) {
-                currentItem.quality = currentItem.quality - 1;
-            }
-            if (isSellInLessThan(currentItem.sellIn, 0)) {
-                if (nameEqual(currentItem, Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    currentItem.quality = 0;
-                }
-                if (nameEqual(currentItem, Constant.AGED_BRIE)
-                        && isQualityLessThan50(currentItem.quality, 50)) {
+            sellInForLagendryItem(currentItem);
+
+            qualityDecreaseForNormalItem(currentItem);
+
+            handleNegativeSellInCases(currentItem);
+        }
+    }
+
+    private boolean isSpecialItem(Item currentItem) {
+        return nameNotEqual(currentItem, Constant.AGED_BRIE)
+                && nameNotEqual(currentItem, Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
+                && qualityGreaterThan(currentItem)
+                && nameNotEqual(currentItem, Constant.SULFURAS_HAND_OF_RAGNAROS);
+    }
+
+    private void handleQualityForNormalItem(Item currentItem) {
+        if (isQualityLessThan50(currentItem.quality, 50)) {
+            currentItem.quality = currentItem.quality + 1;
+            if (currentItem.name.equals(Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
+                    && isSellInLessThan(currentItem.sellIn, 11)
+                    && isQualityLessThan50(currentItem.quality, 50)) {
+                currentItem.quality = currentItem.quality + 1;
+                if (isSellInLessThan(currentItem.sellIn, 6) && isQualityLessThan50(currentItem.quality, 50)) {
                     currentItem.quality = currentItem.quality + 1;
                 }
             }
         }
     }
+
+    private void sellInForLagendryItem(Item currentItem) {
+        if (nameNotEqual(currentItem, Constant.SULFURAS_HAND_OF_RAGNAROS)) {
+            currentItem.sellIn = currentItem.sellIn - 1;
+        }
+    }
+
+    private void handleNegativeSellInCases(Item currentItem) {
+        if (isSellInLessThan(currentItem.sellIn, 0)) {
+            if (nameEqual(currentItem, Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                currentItem.quality = 0;
+            }
+            if (nameEqual(currentItem, Constant.AGED_BRIE)
+                    && isQualityLessThan50(currentItem.quality, 50)) {
+                currentItem.quality = currentItem.quality + 1;
+            }
+        }
+    }
+
+    private void qualityDecreaseForNormalItem(Item currentItem) {
+        if (isSellInLessThan(currentItem.sellIn, 0)
+                && nameNotEqual(currentItem, Constant.AGED_BRIE)
+                && nameNotEqual(currentItem, Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
+                && qualityGreaterThan(currentItem)
+                && nameNotEqual(currentItem, Constant.SULFURAS_HAND_OF_RAGNAROS)) {
+            currentItem.quality = currentItem.quality - 1;
+        }
+    }
+
     private boolean qualityGreaterThan(Item currentItem) {
         return currentItem.quality > 0;
     }
