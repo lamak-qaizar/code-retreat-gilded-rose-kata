34ed6560b0a45dd72cc469ce73f07dd439e08742
Ammar Hasan Razvi <ammar.razvi@careem.com>
2020-07-24 16:18:50 +0500

    extracted quality bounds as constants extracted a method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,10 @@ package com.gildedrose;
 
 class GildedRose {
 
+    public static final int MINIMUM_QUALITY_SCORE = 0;
+    public static final int MAXIMUM_QUALITY_SCORE = 50;
+
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,21 +16,19 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals(ItemType.AGED_BRIE)
                     && !items[i].name.equals(ItemType.BACKSTAGE_PASSES)) {
-                if (items[i].quality > 0) {
+                if (items[i].quality > MINIMUM_QUALITY_SCORE) {
                     if (!items[i].name.equals(ItemType.SULFURAS)) {
                         items[i].quality -= 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < MAXIMUM_QUALITY_SCORE) {
                     items[i].quality += 1;
 
                     if (items[i].name.equals(ItemType.BACKSTAGE_PASSES)) {
                         increaseConcertQualityUnder10Days(i);
 
-                        if (items[i].sellIn < 6) {
-                            increaseQualityUntilMax(i);
-                        }
+                        increasePercentQualityUnder5Days(i, 6);
                     }
                 }
             }
@@ -36,29 +38,33 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(ItemType.AGED_BRIE)) {
-                    if (!items[i].name.equals(ItemType.BACKSTAGE_PASSES)) {
-                        if (items[i].quality > 0) {
+                if (items[i].name.equals(ItemType.AGED_BRIE)) {
+                    if (items[i].quality < MAXIMUM_QUALITY_SCORE) {
+                        items[i].quality += 1;
+                    }
+                } else {
+                    if (items[i].name.equals(ItemType.BACKSTAGE_PASSES)) {
+                        items[i].quality = MINIMUM_QUALITY_SCORE;
+                    } else {
+                        if (items[i].quality > MINIMUM_QUALITY_SCORE) {
                             if (!items[i].name.equals(ItemType.SULFURAS)) {
                                 items[i].quality -= 1;
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
     }
 
-    private void increaseConcertQualityUnder10Days(int i) {
-        if (items[i].sellIn < 11) {
+    private void increasePercentQualityUnder5Days(int i, int i2) {
+        if (items[i].sellIn < i2) {
             increaseQualityUntilMax(i);
         }
+    }
+
+    private void increaseConcertQualityUnder10Days(int i) {
+        increasePercentQualityUnder5Days(i, 11);
     }
 
     private void increaseQualityUntilMax(int i) {
