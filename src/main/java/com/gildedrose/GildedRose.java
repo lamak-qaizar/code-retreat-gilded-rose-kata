11172675e9588c16ac723e7a37a5cadf5408736a
Saad Ismail <saad.ismail@careem.com>
2020-07-24 16:21:05 +0500

    Remove the negate conditions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,11 +12,11 @@ class GildedRose {
 
             if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
 
-            } else if (items[i].name.equals("Aged Brie")){
+            } else if (items[i].name.equals("Aged Brie")) {
 
                 increaseQuality(i);
 
-            } else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
                 increaseQuality(i);
 
@@ -37,18 +37,14 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = 0;
-                    }
-                } else {
+                if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+
+                } else if (items[i].name.equals("Aged Brie")) {
                     increaseQuality(i);
+                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    items[i].quality = 0;
+                } else {
+                    decreaseQuality(i);
                 }
             }
         }
