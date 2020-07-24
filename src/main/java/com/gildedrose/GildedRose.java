04e2311fead2b59d903a9b7761acaf79c84eb36d
ahsan <ahsan.naseem@careem.com>
2020-07-24 17:25:33 +0500

    extracted item logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,63 +6,58 @@ class GildedRose {
     }
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
-            if (!isAged_brie(items[i]) && !isBackstage(items[i] ) && !isSulfuras(items[i])) {
-
-                        items[i].quality = decreaseQualityBy(items[i].quality, 1);
-
-
+            if (isAged_brie(items[i])) {
+                items[i].quality = increaseQualityBy(items[i].quality, 1);
+            } else if (isBackstage(items[i])) {
+                items[i].quality = increaseQualityBy(items[i].quality, 1);
+                incrementBackStageQualitySpecificToSellinDates(items[i]);
+            } else if (isSulfuras(items[i])) {
+                items[i].quality = increaseQualityBy(items[i].quality, 1);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = increaseQualityBy(items[i].quality, 1);
-                    if (isBackstage(items[i])) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = increaseQualityBy(items[i].quality, 1);
-                            }
-                        }
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = increaseQualityBy(items[i].quality, 1);
-                            }
-                        }
-                    }
+                items[i].quality = decreaseQualityBy(items[i].quality, 1);
+            }
+            adjustSellinDays(items[i]);
+            if (items[i].sellIn < 0 && !isSulfuras(items[i])) {
+                if (isAged_brie(items[i])) {
+                    increaseAgedBrieQuality(i);
                 }
-            }
-            if (!isSulfuras(items[i])) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-            if (items[i].sellIn < 0) {
-                if (!isAged_brie(items[i])) {
-                    if (!isBackstage(items[i])) {
-                            if (!isSulfuras(items[i])) {
-                                items[i].quality = decreaseQualityBy(items[i].quality, 1);
-                            }
-
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = increaseQualityBy(items[i].quality, 1);
-                    }
+                else if (isBackstage(items[i])) {
+                    setBackStageQualityToZero(i);
+                }
+                else {
+                    items[i].quality = decreaseQualityBy(items[i].quality, 1);
                 }
             }
         }
     }
-
+    private void setBackStageQualityToZero(int i) {
+        items[i].quality = 0;
+    }
+    private void increaseAgedBrieQuality(int i) {
+        items[i].quality = increaseQualityBy(items[i].quality, 1);
+    }
+    private void adjustSellinDays(Item item) {
+        if (!isSulfuras(item)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+    private void incrementBackStageQualitySpecificToSellinDates(Item item) {
+        if (item.sellIn < 11) {
+            item.quality = increaseQualityBy(item.quality, 1);
+        }
+        if (item.sellIn < 6) {
+            item.quality = increaseQualityBy(item.quality, 1);
+        }
+    }
     private boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
-
     private boolean isBackstage(Item item) {
         return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
-
     private boolean isAged_brie(Item item) {
         return item.name.equals("Aged Brie");
     }
-
     private int decreaseQualityBy(int quality, int offset) {
         if (quality > 0) {
             return quality - offset;
@@ -70,6 +65,7 @@ class GildedRose {
         return quality;
     }
     private int increaseQualityBy(int quality, int offset) {
-        return quality + offset;
+        if (quality < 50) return quality+offset;
+        return quality;
     }
 }
