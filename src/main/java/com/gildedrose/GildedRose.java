ca0b0b452a2047c869a720ad0b84ed511476d0f7
Amir Ali <amir.ali@careem.com>
2020-07-24 16:35:14 +0500

    Refactored into methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,47 +9,60 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(Constant.AGED_BRIE)
-                    && !items[i].name.equals(Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
+            if (nameNotEqual(items[i], Constant.AGED_BRIE)
+                    && nameNotEqual(items[i], Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
                     && items[i].quality > 0
-                    && !items[i].name.equals(Constant.SULFURAS_HAND_OF_RAGNAROS)) {
+                    && nameNotEqual(items[i], Constant.SULFURAS_HAND_OF_RAGNAROS)) {
                 items[i].quality = items[i].quality - 1;
 
             } else {
-                if (items[i].quality < 50) {
+                if (isQualityLessThan50(items[i].quality, 50)) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals(Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
-                            && items[i].sellIn < 11
-                            && items[i].quality < 50) {
+                            && isSellInLessThan(items[i].sellIn, 11)
+                            && isQualityLessThan50(items[i].quality, 50)) {
                         items[i].quality = items[i].quality + 1;
 
-                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                        if (isSellInLessThan(items[i].sellIn, 6) && isQualityLessThan50(items[i].quality, 50)) {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(Constant.SULFURAS_HAND_OF_RAGNAROS)) {
+            if (nameNotEqual(items[i], Constant.SULFURAS_HAND_OF_RAGNAROS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(Constant.AGED_BRIE)) {
-                    if (!items[i].name.equals(Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (items[i].quality > 0 && !items[i].name.equals(Constant.SULFURAS_HAND_OF_RAGNAROS)) {
+            if (isSellInLessThan(items[i].sellIn, 0)) {
+                if (nameNotEqual(items[i], Constant.AGED_BRIE)) {
+                    if (nameNotEqual(items[i], Constant.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (items[i].quality > 0 && nameNotEqual(items[i], Constant.SULFURAS_HAND_OF_RAGNAROS)) {
                             items[i].quality = items[i].quality - 1;
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (isQualityLessThan50(items[i].quality, 50)) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
     }
+
+    private boolean isSellInLessThan(int sellIn, int value) {
+        return sellIn < value;
+    }
+
+    private boolean isQualityLessThan50(int quality, int value) {
+        return quality < value;
+    }
+
+    private boolean nameNotEqual(Item item, String agedBrie) {
+        return !item.name.equals(agedBrie);
+    }
+
 }
