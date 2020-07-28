55f0f57535c523e931f01f57e6a956b971d0e08e
Muhammad Tariq <muhammad.tariq@careem.com>
2020-07-28 17:20:02 +0500

    Moved name comparision into item itself. Temporarily modifying item class.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,10 +9,11 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+            if (!item.isAgedBrie()
+                    && !item.isBackstage()) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!item.isSulfuras()) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -20,7 +21,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.isBackstage()) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -36,15 +37,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!item.isSulfuras()) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!item.isAgedBrie()) {
+                    if (!item.isBackstage()) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!item.isSulfuras()) {
                                 item.quality = item.quality - 1;
                             }
                         }
@@ -59,4 +60,5 @@ class GildedRose {
             }
         }
     }
+
 }
